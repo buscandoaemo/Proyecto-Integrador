395ca0ac65f086e022696c7a995ce8cbd0dfb27c
santichausis <schausis@gmail.com>
2018-03-25 12:15:25 -0300

    Add files via upload

---
+++ php/header.php
@@ -0,0 +1,12 @@
+    <header>
+      <a href="index.php" class="logo"><img src="img/logo.png" alt="The Boarding House Snow & Skate"/></a>
+      <nav>
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="faq.php">FAQ</a></li>
+          <li><a href="registro.php">Registrarse</a></li>
+          <li><a href="login.php">Login</a></li>
+          <li class="cart"><a href="login.php"><i class="icono fas fa-shopping-cart"></i></a></li>
+        </ul>
+      </nav>
+    </header>
